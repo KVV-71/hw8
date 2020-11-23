ae5fa5878426e7e1a063185dfe7c108b02e4a398
KVV-71 <74919676+KVV-71@users.noreply.github.com>
2020-11-23 18:42:40 +0200

    consol input
    consol input

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,15 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using ClassLibrary1;
 
-namespace quadr_equation
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            {
+                double coefficientA = 0;
+                double coefficientB = 0;
+                double coefficientC = 0;
+
+                Console.WriteLine("quadratic equation: ax2 + bx + c = 0");
+                Console.Write("a = ");
+                while (!double.TryParse(Console.ReadLine(), out coefficientA))
+                {
+                    Console.WriteLine("Input Error! Insert number");
+                }
+
+                Console.Write("b = ");
+                while (!double.TryParse(Console.ReadLine(), out coefficientB))
+                {
+                    Console.WriteLine("Input Error! Insert number");
+                }
+
+                Console.Write("c = ");
+                while (!double.TryParse(Console.ReadLine(), out coefficientC))
+                {
+                    Console.WriteLine("Input Error! Insert number");
+                }
+
+                string result = string.Empty;
+
+                if (coefficientA == 0)
+                {
+                    result = "This is not a quadratic equation";
+                }
+                else
+                {
+                    if (coefficientB == 0 || coefficientC == 0)
+                    {
+                        result = Calculator.nQuadraticEquation(coefficientA, coefficientB, coefficientC, result);
+                    }
+                    else
+                    {
+                        result = Calculator.quadraticEquation(coefficientA, coefficientB, coefficientC, result);
+                    }
+                }
+                Console.WriteLine(result);
+
+                Console.ReadKey();
+            }
         }
     }
 }
